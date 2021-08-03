e6a1cb12a7fcd09482bdb9cb3954019339e8b70a
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-08-03 22:51:25 +0200

    implement Resume page

--- src/components/pages/Resume.tsx
+++ src/components/pages/Resume.tsx
@@ -1,8 +1,86 @@
+import { Grid } from '@material-ui/core'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import '../../scss/pages/resume.scss'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload, faSearch } from '@fortawesome/free-solid-svg-icons'
+
+
 function Resume() {
-  return(
-    <div>
-      <h1>Resume</h1>
-    </div>
+  return (
+    <Grid
+      id="resume-container"
+      container
+      justifyContent="center"
+      spacing={2}
+    >
+      {/* headline */}
+      <Grid item xs={12}>
+        <Typography 
+        align="center" 
+        variant="h5"
+        >
+          RESUME
+        </Typography>
+      </Grid>
+
+
+      {/* resume image */}
+      <Grid 
+        item 
+        xs={12} 
+        container 
+        justify="center"
+      >        
+        <img src="./images/resume.png" alt="hhh" width="200px" />
+      </Grid>
+      
+
+      {/* download button */}
+      <Grid 
+        item 
+        xs={6} 
+        container 
+        justifyContent="center"
+      >
+        <Button
+          href="./images/paulina.tech-resume.pdf" 
+          download
+        >
+          <FontAwesomeIcon 
+            size="1x" 
+            icon={faDownload} 
+            color="black" 
+          />
+            &nbsp;
+            Download
+        </Button>
+      </Grid> 
+
+
+      {/* preview button */}
+      <Grid 
+        item 
+        xs={6} 
+        container 
+        justifyContent="center"
+      >
+        <Button
+          href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAEQB0PP0bk&#x2F;view?utm_content=DAEQB0PP0bk&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link" 
+          target="_blank" 
+        >
+          <FontAwesomeIcon 
+            size="1x" 
+            icon={faSearch} 
+            color="black" 
+          />
+            &nbsp;
+            Preview
+        </Button>
+      </Grid> 
+      
+    </Grid>
   )
 }
 
